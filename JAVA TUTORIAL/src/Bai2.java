ed472f04c57c8fbc4dff92d3454769e4102e44aa
khoahner <khoa.hn3092@gmail.com>
2019-09-04 15:14:09 +0700

    Java basic.

---
+++ JAVA TUTORIAL/src/Bai2.java
@@ -0,0 +1,13 @@
+public class Bai2 {
+//Bai2 tên class phải trùng với tên file.java//
+//luôn phải viết hoa chữ cái đầu tiên của tên class đó//
+//Phương thức tương ứng với 1 hành động cụ thể trong ngôn ngữ lập trình//
+//main: nếu muốn ứng dụng chạy được thì phải có 1 phương thức main//
+//Quy ước để gọi 1 phương thức trong Java: object.method(parameter1, parameter 2);//
+    public static void main(String[] args) {
+        System.out.println("thi is first example..");
+        System.out.print("thi is first example..");
+        System.out.print("thi is first example..");
+
+    }
+}
