350276b0c5000be5164ca9b1798b641b59d5f8b2
khoahner <khoa.hn3092@gmail.com>
2019-09-09 15:05:48 +0700

    Value(biến) in Java

---
+++ JAVA TUTORIAL/src/Bai4.java
@@ -0,0 +1,20 @@
+/*Biến trong ngôn ngữ lập trình java
+ - 1 biến: trong máy tính sẽ có những ô nhớ tượng trưng như những cái hộp xếp liền nhau,
+ khi chương trình chạy thì cần dữ liệu đầu vào để xứ lý dữ liệu đầu ra,
+ những giá trị đấy được lưu trữ trong 1 vùng nhớ được đặt tên,
+ tên của 1 cụm các ô nhớ (cụm ô nhớ có thể gồm 1 hoặc nhiều ô nhớ)
+ - Biến là tên của 1 vùng nhớ được dùng để lưu trữ dữ liệu.
+ - Với mỗi 1 biến fai xác định 1 kiểu.
+ */
+public class Bai4 {
+    public static void main(String[] args) {
+    int firstnumber = 100; //khai báo biến//
+    int secondnumber = 200; //khai báo biến//
+    int sum = firstnumber + secondnumber; //khai báo biến//
+    long myValue;
+    boolean IsTrue;
+    char character = '1'; //khởi tạo: khai báo biến đồng thời gán giá trị cho biến//
+    double laisuat;
+        System.out.println("SUM OF TWO NUMBER: " +(firstnumber+secondnumber)+","+character);
+    }
+}
