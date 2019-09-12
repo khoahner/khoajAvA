bf3d393cbb2e837ea9109847d441edcc32a6ea41
khoahner <khoa.hn3092@gmail.com>
2019-09-12 17:37:19 +0700

    comment in Java - // - /**/ - /**

---
+++ JAVA TUTORIAL/src/Bai6.java
@@ -0,0 +1,15 @@
+
+comment trong Java
+
+        comment 1 dòng: //
+
+        comment nhiều dòng: /* ..v.
+                    vv. */
+
+        comment tài liệu document : /**
+
+
+
+
+public class Bai6 {
+}
