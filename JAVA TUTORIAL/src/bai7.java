7ae96e2df37fe6e1416147c6af0d9dcb45405594
khoahner <khoa.hn3092@gmail.com>
2019-09-23 15:54:49 +0700

    Some special characters in java

---
+++ JAVA TUTORIAL/src/bai7.java
@@ -0,0 +1,15 @@
+
+//các kí tự có chức nặng đặt biệt trong Java
+/**
+ *  \n: cho phép xuống dòng
+ *  \b: xóa bỏ 1 kí tự từ cuối dãy
+ *  \": tạo nháy kép
+ *  \': tạo dấu nháy đơn
+ *  \\: tạo dấu \
+ *  \t: tab ra 1 khoãng
+ */
+public class bai7 {
+    public static void main(String[] args) {
+        System.out.println("\"Java\"\n\'Tutorial\'\n07\b\b\nh\te\tl\tl\to");
+    }
+}
